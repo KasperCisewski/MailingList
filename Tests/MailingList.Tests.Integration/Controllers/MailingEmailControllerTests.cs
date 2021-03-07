edcdb5ecabdd92a8dc10327933c01467423b3497
Kasper Cisewski <kaspercisewski@gmail.com>
2021-03-07 23:48:25 +0100

    Added base classes for integration tests

---
+++ Tests/MailingList.Tests.Integration/Controllers/MailingEmailControllerTests.cs
@@ -0,0 +1,8 @@
+﻿using MailingList.Tests.Integration.Controllers.Base;
+
+namespace MailingList.Tests.Integration.Controllers
+{
+    class MailingEmailControllerTests : BaseController
+    {
+    }
+}
